7ae41739c95764f348ca2841fd957466480e65f7
rana <mdiqbalhussian752@gmail.com>
2022-08-02 02:28:48 +0800

    gradeing system pogram

---
+++ modul-17-plmslove-2.js
@@ -0,0 +1,89 @@
+// modul - 17 - plmslove - 2 
+
+
+var iqbal = 99;
+var hossen = 66;
+var rana = 95;
+var peter = 56;
+var jhon = 40;
+
+
+if (iqbal >= 80) {
+    console.log("A Grade");
+}
+else if (iqbal >= 70) {
+    console.log("B Grade");
+}
+else if (iqbal >= 60) {
+    console.log("c Grade");
+}
+else if (iqbal >= 50) {
+    console.log("D Grade");
+}
+else {
+    console.log("F grade");
+}
+if (hossen >= 80) {
+    console.log("A Grade");
+}
+else if (hossen >= 70) {
+    console.log("B Grade");
+}
+else if (hossen >= 60) {
+    console.log("c Grade");
+}
+else if (hossen >= 50) {
+    console.log("D Grade");
+}
+else {
+    console.log("F grade");
+}
+
+if (rana >= 80) {
+    console.log("A Grade");
+}
+else if (rana >= 70) {
+    console.log("B Grade");
+}
+else if (rana >= 60) {
+    console.log("c Grade");
+}
+else if (rana >= 50) {
+    console.log("D Grade");
+}
+else {
+    console.log("F grade");
+}
+
+if (peter >= 80) {
+    console.log("A Grade");
+}
+else if (peter >= 70) {
+    console.log("B Grade");
+}
+else if (peter >= 60) {
+    console.log("c Grade");
+}
+else if (peter >= 50) {
+    console.log("D Grade");
+}
+else {
+    console.log("F grade");
+}
+if (jhon >= 80) {
+    console.log("A Grade");
+}
+else if (jhon >= 70) {
+    console.log("B Grade");
+}
+else if (jhon >= 60) {
+    console.log("c Grade");
+}
+else if (jhon >= 50) {
+    console.log("D Grade");
+}
+else {
+    console.log("F grade");
+}
+
+
