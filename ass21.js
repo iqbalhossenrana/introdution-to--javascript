62212d784300865a38395626e2babd1733a1ce39
rana <mdiqbalhussian752@gmail.com>
2022-08-02 00:53:25 +0800

    modul-17 plm slove added

---
+++ ass21.js
@@ -0,0 +1,37 @@
+// module 17 practic problem-2 
+
+var fruits = ["appal", "Banana", "orange"];
+
+// fruits.push("mango");
+
+fruits.splice(1, 1, 'mango');
+
+console.log(fruits);
+
+fruits.pop("orange");
+console.log(fruits);
+
+fruits.push("watermelon");
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+// fruits.indexOf("Banana");
+
+// console.log(fruits.indexOf("Banana"));
+// console.log(fruits.push("mango"));
+
+// var index = fruits.indexOf(1);
+
+// fruits.push("mango");
+// fruits.splice(index, 1);
+
+// console.log(fruits.splice(index, 1));
+
+
