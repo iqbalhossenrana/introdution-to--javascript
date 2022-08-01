7e5631d89775be1280127042a1fb23585dbd4461
rana <mdiqbalhussian752@gmail.com>
2022-08-02 04:33:47 +0800

    large number find provlem slov

---
+++ modul-17-plmslove-3.js
@@ -0,0 +1,14 @@
+// console.log(100);
+
+var num1 = 79;
+var num2 = 13;
+var num3 = 45;
+if (num1 > num2 && num1 > num3) {
+    console.log('num1 is the largest')
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log('num2 is the largest')
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log('num3 is the largest')
+}
